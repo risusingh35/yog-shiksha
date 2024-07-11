833d5bc6d6a9b9674f97df4f289eab646bf74831
Parth Mehta <parthmehta@moaiconsulting.co.in>
2024-07-11 17:29:35 +0530

    highChard -pie and donut on dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,253 @@
-import React from 'react';
-const Dashboard: React.FC = () => {
+import PageTitleBar from "@/component/pageTitleBar/PageTitleBar";
+import Spinner from "@/component/spinner/Spinner";
+import { FC, useState, useEffect, useRef } from "react";
+import Highcharts, { Options, Chart } from "highcharts";
+import HighchartsReact, { HighchartsReactRefObject } from "highcharts-react-official";
+
+const Dashboard: FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const donutChartRef1 = useRef<HighchartsReactRefObject>(null);
+  const donutChartRef2 = useRef<HighchartsReactRefObject>(null);
+
+  const drawTotal = (chart: Chart) => {
+    const total = chart.series[0].data.reduce((acc, point) => acc + (point.y ? point.y:0), 0);
+    chart.renderer
+      .text(
+        `Total: ${total}`,
+        100,
+        chart.plotHeight / 2 + chart.plotTop
+      )
+      .css({
+        color: "#000",
+        fontSize: "16px",
+        fontWeight: "bold",
+        textAlign: "center",
+      })
+      .attr({ zIndex: 5 })
+      .add();
+  };
+
+  useEffect(() => {
+    if (donutChartRef1.current && donutChartRef1.current.chart) {
+      drawTotal(donutChartRef1.current.chart);
+    }
+    if (donutChartRef2.current && donutChartRef2.current.chart) {
+      drawTotal(donutChartRef2.current.chart);
+    }
+  }, []);
+
+  const data1 = [
+    { name: "Active", y: 30 },
+    { name: "Subscription", y: 50 },
+    { name: "Delete", y: 64 },
+    { name: "Inactive", y: 80 },
+  ];
+  const data2 = [
+    { name: "Active", y: 20 },
+    { name: "Subscription", y: 50 },
+    { name: "Delete", y: 40 },
+    { name: "Inactive", y: 50 },
+  ];
+  const data3 = [
+    { name: "Active", y: 30 },
+    { name: "Subscription", y: 50 },
+    { name: "Delete", y: 60 },
+    { name: "Inactive", y: 50 },
+  ];
+  const data4 = [
+    { name: "Active", y: 40 },
+    { name: "Subscription", y: 50 },
+    { name: "Delete", y: 24 },
+    { name: "Inactive", y: 120 },
+  ];
+const pieDataLabelsStyle={
+  fontWeight: "bold",
+  color: "black",
+  fontSize: "1rem",
+  textOutline: "none",
+}
+const donutDataLabelsStyle={
+  fontWeight: "bold",
+  color: "black",
+  fontSize: "0.8rem",
+  textOutline: "none",
+}
+const piePlotOptions={
+  pie: {
+    allowPointSelect: true,
+    cursor: "pointer",
+    dataLabels: {
+      enabled: true,
+      distance: -50,
+      format: "{point.percentage:.1f} %",
+      style:pieDataLabelsStyle,
+    },
+  },
+}
+const donutPlotOptions={
+  pie: {
+    allowPointSelect: true,
+    cursor: "pointer",
+    dataLabels: {
+      enabled: true,
+      distance: -35,
+      format: "{point.percentage:.1f}%",
+      style: donutDataLabelsStyle,
+    },
+    // center: ["50%", "50%"],
+    // borderWidth: 3,
+  },
+  series: {
+    animation: false,
+    showInLegend: true,
+  },
+}
+  const options1: Options = {
+    chart: {
+      type: "pie",
+    },
+    credits: {
+      enabled: false,
+    },
+    title: {
+      text: "Users",
+    },
+    subtitle: {
+      text: "Active, Subscription, Delete, and Inactive",
+    },
+    plotOptions: piePlotOptions,
+    series: [
+      {
+        type: "pie",
+        name: "Users",
+        data: data1,
+      },
+    ],
+  };
+
+  const options2: Options = {
+    chart: {
+      type: "pie",
+    },
+    credits: {
+      enabled: false,
+    },
+    title: {
+      text: "Users",
+    },
+    subtitle: {
+      text: "Active, Subscription, Delete, and Inactive",
+    },
+    plotOptions:piePlotOptions,
+    series: [
+      {
+        type: "pie",
+        name: "Users",
+        data: data2,
+      },
+    ],
+  };
+
+  const donutChartOptions1: Options = {
+    colors: ["#01BAF2", "#71BF45", "#FAA74B", "#B37CD2"],
+    chart: {
+      type: "pie",
+    },
+    credits: {
+      enabled: false,
+    },
+    accessibility: {
+      point: {
+        valueSuffix: "%",
+      },
+    },
+    title: {
+      text: "Users",
+    },
+    subtitle: {
+      text: "Active, Subscription, Delete, and Inactive",
+    },
+    tooltip: {
+      pointFormat: "<b>{point.name}: {point.y}</b>",
+    },
+    plotOptions: donutPlotOptions,
+    series: [
+      {
+        type: "pie",
+        name: "Users",
+        size: "100%",
+        innerSize: "50%",
+        data: data3,
+      },
+    ],
+  };
+
+  const donutChartOptions2: Options = {
+    colors: ["#01BAF2", "#71BF45", "#FAA74B", "#B37CD2"],
+    chart: {
+      type: "pie",
+    },
+    credits: {
+      enabled: false,
+    },
+    accessibility: {
+      point: {
+        valueSuffix: "%",
+      },
+    },
+    title: {
+      text: "Users",
+    },
+    subtitle: {
+      text: "Active, Subscription, Delete, and Inactive",
+    },
+    tooltip: {
+      pointFormat: "<b>{point.name}: {point.y}</b>",
+    },
+    plotOptions: donutPlotOptions,
+    series: [
+      {
+        type: "pie",
+        name: "Users",
+        size: "100%",
+        innerSize: "50%",
+        data: data4,
+      },
+    ],
+  };
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard page.</p>
+    <div className="flex flex-col h-full">
+      <Spinner
+        text="Loading..."
+        closedIn={15000}
+        onClose={() => setIsLoading(false)}
+        isVisible={isLoading}
+      />
+      <PageTitleBar title="Dashboard" />
+      <div className="flex flex-wrap justify-around">
+        <div className="w-full sm:w-1/1 md:w-1/2 lg:w-1/4 p-2">
+          <HighchartsReact highcharts={Highcharts} options={options1} />
+        </div>
+        <div className="w-full sm:w-1/1 md:w-1/2 lg:w-1/4 p-2">
+          <HighchartsReact highcharts={Highcharts} options={options2} />
+        </div>
+        <div className="w-full sm:w-1/1 md:w-1/2 lg:w-1/4 p-2">
+          <HighchartsReact
+            ref={donutChartRef1}
+            highcharts={Highcharts}
+            options={donutChartOptions1}
+          />
+        </div>
+      
+        <div className="w-full sm:w-1/1 md:w-1/2 lg:w-1/4 p-2">
+          <HighchartsReact
+            ref={donutChartRef2}
+            highcharts={Highcharts}
+            options={donutChartOptions2}
+          />
+        </div>
+      </div>
     </div>
   );
 };
